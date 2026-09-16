656faa3d40b8775e886afea0516c050195ee755a
Suriya Sri S <suriyasri2006@gmail.com>
2026-09-16 08:46:45 +0530

    Delete InfosyssprinboardInternshiphealthcareAI-Agent/fix_rls.sql

--- InfosyssprinboardInternshiphealthcareAI-Agent/fix_rls.sql
+++
@@ -1,3 +0,0 @@
-DROP POLICY IF EXISTS ""Doctors can view all users"" ON public.users;
-DROP POLICY IF EXISTS ""Allow authenticated to read users"" ON public.users;
-CREATE POLICY ""Allow authenticated to read users"" ON public.users FOR SELECT USING (auth.role() = 'authenticated');
